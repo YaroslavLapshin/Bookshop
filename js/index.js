25bb5e7856d3460396d7b1a5b6f8e8f40446c87e
YaroslavLapshin <84934307+YaroslavLapshin@users.noreply.github.com>
2024-07-30 17:46:31 +0000

    as

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
-function init() {
-    import('./carousel-top.js')
-    import('./carousel-books.js');
-}
+// function init() {
+//     import('./carousel-top.js')
+//     import('./carousel-books.js');
+//     import('./header-burger.js');
+// }
 
 
-const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
-let loadedPartialsCount = 0;
+// const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+// let loadedPartialsCount = 0;
 
-document.body.addEventListener('htmx:afterOnLoad', () => {
-    loadedPartialsCount++;
-    if (loadedPartialsCount === totalPartials) init();
-});
+// document.body.addEventListener('htmx:afterOnLoad', () => {
+//     loadedPartialsCount++;
+//     if (loadedPartialsCount === totalPartials) init();
+// });
+
+const menuBtn = document.querySelector('.menu__button');
+const menu = document.querySelector('.header__nav');
+            
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.add('active');
+    menu.classList.remove('active');
+})
